10630c6feba71d506c91adf9189ee19053fc4c38
Paul Campbell <pcampbell@kemitix.net>
2020-08-16 12:36:30 +0100

    Log environment variables used

--- slushy/src/main/java/net/kemitix/slushy/QuarkusSlushyConfig.java
+++ slushy/src/main/java/net/kemitix/slushy/QuarkusSlushyConfig.java
@@ -2,10 +2,13 @@ package net.kemitix.slushy;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.java.Log;
 import net.kemitix.slushy.spi.SlushyConfig;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
+@Log
 @Setter
 @Getter
 @ApplicationScoped
@@ -18,5 +21,15 @@ public class QuarkusSlushyConfig
     private String boardName = System.getenv("SLUSHY_BOARD");
     private String sender = System.getenv("SLUSHY_SENDER");
     private String reader = System.getenv("SLUSHY_READER");
+
+    @PostConstruct
+    void init() {
+        log.info(String.format("TRELLO_KEY: %s", trelloKey));
+        log.info(String.format("TRELLO_SECRET: %b", trelloSecret.length() > 0));
+        log.info(String.format("SLUSHY_USER: %s", userName));
+        log.info(String.format("SLUSHY_BOARD: %s", boardName));
+        log.info(String.format("SLUSHY_SENDER: %s", sender));
+        log.info(String.format("SLUSHY_READER: %s", reader));
+    }
 
 }
