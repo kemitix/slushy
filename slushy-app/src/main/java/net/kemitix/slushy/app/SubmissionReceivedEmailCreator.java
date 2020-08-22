3d37af26929d34cf06aa639fc349a6935c934ce8
Paul Campbell <pcampbell@kemitix.net>
2020-08-22 15:03:48 +0100

    merge SubmissionReceived*Creator classes into one class

--- slushy-app/src/main/java/net/kemitix/slushy/app/SubmissionReceivedEmailCreator.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/SubmissionReceivedEmailCreator.java
@@ -3,8 +3,16 @@ package net.kemitix.slushy.app;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class SubmissionReceivedBodyCreator
-        implements BodyCreator<Submission> {
+public class SubmissionReceivedEmailCreator
+        implements SubjectCreator<Submission>, BodyCreator<Submission> {
+
+    @Override
+    public String subject(Submission submission) {
+        return String.format(
+                "Submission Received: %s by %s",
+                submission.getTitle(),
+                submission.getByline());
+    }
 
     @Override
     public String bodyText(Submission submission) {
