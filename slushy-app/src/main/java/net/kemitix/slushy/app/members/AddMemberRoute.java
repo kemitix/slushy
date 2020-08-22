d31e96f5ac981e5b676528f5bca8644869ca3412
Paul Campbell <pcampbell@kemitix.net>
2020-08-22 16:43:02 +0100

    Add user to cards when moved to Slush

---
+++ slushy-app/src/main/java/net/kemitix/slushy/app/members/AddMemberRoute.java
@@ -0,0 +1,26 @@
+package net.kemitix.slushy.app.members;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import static org.apache.camel.builder.Builder.bean;
+
+@ApplicationScoped
+public class AddMemberRoute
+        extends RouteBuilder {
+
+    @Inject AddMember addMember;
+
+    @Override
+    public void configure() {
+        from("direct:Slushy.AddMember")
+                .routeId("Slushy.AddMember")
+                .setHeader("Slushy.Inbox.Card",
+                        bean(addMember,
+                                "addToCard(${header[Slushy.Inbox.Card]})"))
+        ;
+    }
+
+}
