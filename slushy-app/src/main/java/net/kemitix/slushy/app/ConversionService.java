20352b5f4440a13012e393970600ddb44db10f41
Paul Campbell <pcampbell@kemitix.net>
2020-08-23 17:09:54 +0100

    ConversionService.canConvert: checks if a valid converter exists

--- slushy-app/src/main/java/net/kemitix/slushy/app/ConversionService.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/ConversionService.java
@@ -3,6 +3,7 @@ package net.kemitix.slushy.app;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
+import java.io.File;
 
 @ApplicationScoped
 public class ConversionService {
@@ -16,6 +17,21 @@ public class ConversionService {
                 .findFirst()
                 .flatMap(converter -> converter.convert(attachment))
                 .orElse(attachment);
+    }
+
+    public boolean canConvert(String filename) {
+        Attachment attachment = new Attachment() {
+            @Override
+            public File getFileName() {
+                return new File(filename);
+            }
+            @Override
+            public Attachment download() {
+                return null;
+            }
+        };
+        return attachmentConverters.stream()
+                .anyMatch(converter -> converter.canHandle(attachment));
     }
 
 }
