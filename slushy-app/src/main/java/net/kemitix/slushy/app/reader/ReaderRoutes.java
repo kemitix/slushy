a9061af795487ff5903878f12c6f644f284b879b
Paul Campbell <pcampbell@kemitix.net>
2020-09-03 08:39:17 +0100

    Poll the Reader list

--- slushy-app/src/main/java/net/kemitix/slushy/app/reader/ReaderRoutes.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/reader/ReaderRoutes.java
@@ -1,6 +1,7 @@
 package net.kemitix.slushy.app.reader;
 
 import net.kemitix.slushy.app.CardMover;
+import net.kemitix.slushy.app.trello.TrelloBoard;
 import org.apache.camel.builder.RouteBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -11,10 +12,19 @@ public class ReaderRoutes
         extends RouteBuilder {
 
     @Inject ReaderConfig readerConfig;
+    @Inject TrelloBoard trelloBoard;
     @Inject CardMover cardMover;
 
     @Override
     public void configure() {
+        fromF("timer:reader?period=%s", readerConfig.getScanPeriod())
+                .routeId("Slushy.Reader")
+                .setBody(exchange -> trelloBoard.getListCards(readerConfig.getSourceList()))
+                .split(body())
+                .setHeader("Slushy.RoutingSlip", readerConfig::getRoutingSlip)
+                .routingSlip(header("Slushy.RoutingSlip"))
+        ;
+
         from("direct:Slushy.Reader.MoveToTargetList")
                 .routeId("Slushy.Reader.MoveToTargetList")
                 .setHeader("Slushy.TargetList", readerConfig::getTargetList)
