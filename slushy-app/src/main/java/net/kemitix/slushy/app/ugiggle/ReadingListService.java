a6d360c44562fdf2aa3ffc424c55553c02fa4b6a
Paul Campbell <pcampbell@kemitix.net>
2020-09-04 08:38:22 +0100

    ugiggle: Remove remaining unused package

--- slushy-app/src/main/java/net/kemitix/slushy/app/ugiggle/ReadingListService.java
+++
@@ -1,9 +0,0 @@
-package net.kemitix.slushy.app.ugiggle;
-
-import net.kemitix.slushy.app.trello.TrelloCard;
-
-import java.util.stream.Stream;
-
-public interface ReadingListService {
-    Stream<TrelloCard> getReadingList();
-}
