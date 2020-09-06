6175c4ac7ec5bafe6b58a84eaac19d360801c5ee
Paul Campbell <pcampbell@kemitix.net>
2020-09-06 14:02:56 +0100

    badattachement: convert to velocity template

--- slushy-app/src/main/java/net/kemitix/slushy/app/badattachment/InvalidAttachmentRoute.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/badattachment/InvalidAttachmentRoute.java
@@ -1,15 +1,13 @@
 package net.kemitix.slushy.app.badattachment;
 
 import net.kemitix.slushy.app.Comments;
-import net.kemitix.slushy.app.email.EmailService;
 import net.kemitix.slushy.app.SlushyConfig;
-import net.kemitix.slushy.app.templating.SlushyTemplate;
+import net.kemitix.slushy.app.ValidFileTypes;
+import net.kemitix.slushy.app.email.EmailService;
 import org.apache.camel.builder.RouteBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-
-import static org.apache.camel.builder.Builder.bean;
 
 @ApplicationScoped
 public class InvalidAttachmentRoute
@@ -18,29 +16,26 @@ public class InvalidAttachmentRoute
     @Inject SlushyConfig slushyConfig;
     @Inject EmailService emailService;
     @Inject Comments comments;
-    @Inject SlushyTemplate slushyTemplate;
-    @Inject BadAttachmentModelBuilder badAttachmentModelBuilder;
+    @Inject ValidFileTypes validFileTypes;
 
     @Override
     public void configure() {
         from("direct:Slushy.InvalidAttachment")
                 .routeId("Slushy.InvalidAttachment")
                 .log("Submission rejected due to an unsupported file type")
+
+                .setHeader("SlushyValidFileTypes", () ->
+                        String.join(", ", validFileTypes.get()))
+
                 // send email to author
                 .setHeader("SlushyRecipient").simple("${header.SlushySubmission.email}")
                 .setHeader("SlushySender", slushyConfig::getSender)
-
-                .setHeader("SlushyTemplateModel", bean(badAttachmentModelBuilder))
-
-                .setHeader("SlushyTemplateName").simple("badattachment/subject.txt")
-                .setHeader("SlushySubject", bean(slushyTemplate))
-
-                .setHeader("SlushyTemplateName", simple("badattachment/body.txt"))
-                .setHeader("SlushyBody", bean(slushyTemplate))
-
-                .setHeader("SlushyTemplateName", simple("badattachment/body.html"))
-                .setHeader("SlushyBodyHtml", bean(slushyTemplate))
-
+                .to("velocity:net/kemitix/slushy/app/badattachment/subject.txt")
+                .setHeader("SlushySubject").body()
+                .to("velocity:net/kemitix/slushy/app/badattachment/body.txt")
+                .setHeader("SlushyBody").body()
+                .to("velocity:net/kemitix/slushy/app/badattachment/body.html")
+                .setHeader("SlushyBodyHtml").body()
                 .bean(emailService,
                         "send(" +
                                 "${header.SlushyRecipient}, " +
@@ -48,14 +43,16 @@ public class InvalidAttachmentRoute
                                 "${header.SlushySubject}, " +
                                 "${header.SlushyBody}, " +
                                 "${header.SlushyBodyHtml})")
-                .setHeader("SlushyComment",
-                        () -> "Sent invalid attachment rejection notification to author")
+
+                .setHeader("SlushyComment").simple(
+                        "Sent invalid attachment rejection notification to author")
                 .bean(comments, "add(" +
                         "${header.SlushyCard}, " +
                         "${header.SlushyComment}" +
                         ")")
+
                 // move card to rejected
-                .to("direct:Slushy.Reject.MoveToRejected")
+                .to("direct:Slushy.Reject.MoveToTargetList")
         ;
     }
 
