975706d896c05a3046b9a6296f5f2419dc1378e6
Paul Campbell <pcampbell@kemitix.net>
2020-09-07 21:04:55 +0100

    First attempt at creating a route template
    Blocked from running until Quarkus dependency for Camel is upgrade to 3.5.0

--- slushy-app/src/main/java/net/kemitix/slushy/app/inbox/InboxRoutes.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/inbox/InboxRoutes.java
@@ -1,18 +1,11 @@
 package net.kemitix.slushy.app.inbox;
 
 import net.kemitix.slushy.app.MoveCard;
-import net.kemitix.slushy.app.AddComment;
-import net.kemitix.slushy.app.IsRequiredAge;
 import net.kemitix.slushy.app.OnException;
-import net.kemitix.slushy.app.SlushyConfig;
 import net.kemitix.slushy.app.ParseSubmission;
-import net.kemitix.slushy.app.email.SendEmail;
-import net.kemitix.slushy.app.trello.TrelloBoard;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.TemplatedRouteBuilder;
-
-import java.io.IOException;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -24,26 +17,20 @@ import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_ROUTING_SLIP
 import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_SCAN_PERIOD;
 import static net.kemitix.slushy.app.ListProcessRouteTemplate.ROUTE_LIST_PROCESS;
 
-import static org.apache.camel.builder.Builder.bean;
-
 @ApplicationScoped
 public class InboxRoutes
         extends RouteBuilder {
 
-    @Inject SlushyConfig slushyConfig;
     @Inject InboxConfig inboxConfig;
-
     @Inject ParseSubmission parseSubmission;
     @Inject ReformatCard reformatCard;
     @Inject MoveCard moveCard;
-    @Inject SendEmail sendEmail;
-    @Inject AddComment addComment;
     @Inject CamelContext camelContext;
 
     @Override
     public void configure() {
         OnException.retry(this, inboxConfig);
-      
+
         TemplatedRouteBuilder.builder(camelContext, ROUTE_LIST_PROCESS)
                 .parameter(PARAM_NAME, "inbox")
                 .parameter(PARAM_SCAN_PERIOD, inboxConfig.getScanPeriod())
