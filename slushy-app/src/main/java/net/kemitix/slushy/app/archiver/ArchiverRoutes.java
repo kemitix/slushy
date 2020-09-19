1ad5b4c0980b7fffd3114f22e5aff4241e6941bb
Paul Campbell <pcampbell@kemitix.net>
2020-09-19 10:18:43 +0100

    ArchiverRoutes uses ListProcessTemplate

--- slushy-app/src/main/java/net/kemitix/slushy/app/archiver/ArchiverRoutes.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/archiver/ArchiverRoutes.java
@@ -1,38 +1,39 @@
 package net.kemitix.slushy.app.archiver;
 
-import net.kemitix.slushy.app.IsRequiredAge;
 import net.kemitix.slushy.app.OnException;
-import net.kemitix.slushy.app.trello.TrelloBoard;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.TemplatedRouteBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import static org.apache.camel.builder.Builder.bean;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_LIST_NAME;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_NAME;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_REQUIRED_AGE_HOURS;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_ROUTING_SLIP;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.PARAM_SCAN_PERIOD;
+import static net.kemitix.slushy.app.ListProcessRouteTemplate.ROUTE_LIST_PROCESS;
 
 @ApplicationScoped
 public class ArchiverRoutes
         extends RouteBuilder {
 
     @Inject ArchiverConfig archiverConfig;
-    @Inject TrelloBoard trelloBoard;
-    @Inject IsRequiredAge isRequiredAge;
     @Inject ArchiveCard archiveCard;
+    @Inject CamelContext camelContext;
 
     @Override
     public void configure() {
         OnException.retry(this, archiverConfig);
 
-        fromF("timer:archiver?period=%s", archiverConfig.getScanPeriod())
-                .routeId("Slushy.Archiver")
-                .setBody(exchange -> trelloBoard.getListCards(archiverConfig.getSourceList()))
-                .split(body())
-
-                .setHeader("SlushyRequiredAge", archiverConfig::getRequiredAgeHours)
-                .filter(bean(isRequiredAge))
-
-                .setHeader("SlushyRoutingSlip", archiverConfig::getRoutingSlip)
-                .routingSlip(header("SlushyRoutingSlip"))
+        TemplatedRouteBuilder.builder(camelContext, ROUTE_LIST_PROCESS)
+                .parameter(PARAM_NAME, "archiver")
+                .parameter(PARAM_SCAN_PERIOD, archiverConfig.getScanPeriod())
+                .parameter(PARAM_LIST_NAME, archiverConfig.getSourceList())
+                .parameter(PARAM_REQUIRED_AGE_HOURS, archiverConfig.getRequiredAgeHours())
+                .parameter(PARAM_ROUTING_SLIP, archiverConfig.getRoutingSlip())
+                .add()
         ;
 
         from("direct:Slushy.ArchiveCard")
