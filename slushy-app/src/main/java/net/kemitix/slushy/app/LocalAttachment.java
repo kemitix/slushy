ea008625cd9f26ace3475f2882026266e9aff813
Paul Campbell <pcampbell@kemitix.net>
2020-10-04 09:48:06 +0100

    LocalAttachment: add downloaded file length

--- slushy-app/src/main/java/net/kemitix/slushy/app/LocalAttachment.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/LocalAttachment.java
@@ -15,13 +15,16 @@ public class LocalAttachment
 
     private final File filename;
     private final File originalFilename;
+    private final long length;
 
     public LocalAttachment(
             File filename,
-            File originalFilename
+            File originalFilename,
+            long length
     ) {
         this.filename = filename;
         this.originalFilename = originalFilename;
+        this.length = length;
     }
 
     @Override
