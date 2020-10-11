38149d2ea1c59c8edc2357064c88c902906e0840
Paul Campbell <pcampbell@kemitix.net>
2020-10-11 13:16:18 +0100

    archiver: split routes into own classes

---
+++ slushy-app/src/main/java/net/kemitix/slushy/app/archiver/ArchiveCardRoute.java
@@ -0,0 +1,34 @@
+package net.kemitix.slushy.app.archiver;
+
+import net.kemitix.slushy.app.OnException;
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class ArchiveCardRoute
+        extends RouteBuilder {
+
+    private final ArchiverConfig archiverConfig;
+    private final ArchiveCard archiveCard;
+
+    @Inject
+    public ArchiveCardRoute(
+            ArchiverConfig archiverConfig,
+            ArchiveCard archiveCard
+    ) {
+        this.archiverConfig = archiverConfig;
+        this.archiveCard = archiveCard;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        OnException.retry(this, archiverConfig);
+
+        from("direct:Slushy.ArchiveCard")
+                .routeId("Slushy.ArchiveCard")
+                .bean(archiveCard)
+        ;
+    }
+}
