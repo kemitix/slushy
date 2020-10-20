caddcc693082ea9287ad5aa0e29332585b98e487
Paul Campbell <pcampbell@kemitix.net>
2020-10-20 15:18:17 +0100

    api/topItem: Use JaxRS to provide endpoint

---
+++ slushy-api/src/main/java/net/kemitix/slushy/api/SlushyTopItem.java
@@ -0,0 +1,42 @@
+package net.kemitix.slushy.api;
+
+import lombok.extern.java.Log;
+import net.kemitix.slushy.app.ParseSubmission;
+import net.kemitix.slushy.app.Submission;
+import net.kemitix.slushy.app.reader.ReaderConfig;
+import net.kemitix.slushy.app.trello.TrelloBoard;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log
+@ApplicationScoped
+public class SlushyTopItem {
+
+    private final TrelloBoard trelloBoard;
+    private final ReaderConfig readerConfig;
+    private final ParseSubmission parseSubmission;
+
+    @Inject
+    public SlushyTopItem(
+            TrelloBoard trelloBoard,
+            ReaderConfig readerConfig,
+            ParseSubmission parseSubmission
+    ) {
+        this.trelloBoard = trelloBoard;
+        this.readerConfig = readerConfig;
+        this.parseSubmission = parseSubmission;
+    }
+
+    public List<Submission> topItem() {
+        return trelloBoard.getListCards(readerConfig.getTargetList())
+                .stream()
+                .findFirst()
+                .map(parseSubmission::parse)
+                .stream()
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ slushy-api/src/main/java/net/kemitix/slushy/api/SlushyTopItemResource.java
@@ -0,0 +1,28 @@
+package net.kemitix.slushy.api;
+
+import net.kemitix.slushy.app.Submission;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/slush/topitem")
+public class SlushyTopItemResource {
+
+    private final SlushyTopItem slushyTopItem;
+
+    @Inject
+    public SlushyTopItemResource(SlushyTopItem slushyTopItem) {
+        this.slushyTopItem = slushyTopItem;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Submission> get() {
+        return slushyTopItem.topItem();
+    }
+
+}
