889fa71245f0ea67f5906afc80eab7849e95975b
Paul Campbell <pcampbell@kemitix.net>
2020-11-16 22:44:10 +0000

    duedates: rename route as HoldDecisionDueBy

--- slushy/src/main/java/net/kemitix/slushy/QuarkusHoldConfig.java
+++ slushy/src/main/java/net/kemitix/slushy/QuarkusHoldConfig.java
@@ -1,11 +1,17 @@
 package net.kemitix.slushy;
 
 import io.quarkus.arc.config.ConfigProperties;
+import lombok.Getter;
+import lombok.Setter;
 import net.kemitix.slushy.app.hold.HoldConfig;
 
+@Setter
+@Getter
 @ConfigProperties(prefix = "slushy.hold")
 public class QuarkusHoldConfig
         extends AbstractQuarkusListProcessingConfig
         implements HoldConfig {
+
+    private long dueDays;
 
 }
