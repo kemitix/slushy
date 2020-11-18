687de5a42f5aae6cada10479544e97fe80dc0200
Paul Campbell <pcampbell@kemitix.net>
2020-11-18 21:32:53 +0000

    app.trello.queue: detect which card moved from which list to another

--- slushy-app/src/main/java/net/kemitix/slushy/app/trello/queue/WebHookTrelloRoute.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/trello/queue/WebHookTrelloRoute.java
@@ -1,6 +1,7 @@
 package net.kemitix.slushy.app.trello.queue;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -11,10 +12,35 @@ public class WebHookTrelloRoute
     public void configure() throws Exception {
         from("direct:Slushy.WebHook.Trello")
                 .routeId("Slushy.WebHook.Trello")
-                .log("SOURCE is Trello")
 
+                .idempotentConsumer()
+                .jsonpath("body-json.action.id")
+                .skipDuplicate(true)
+                .messageIdRepository(MemoryIdempotentRepository::new)
+
+                .log("WebHook message from Trello")
+
+//                .process(exchange ->
+//                        log.info(exchange.getIn().getBody(String.class)))
+
+                .choice()
+
+                .when().jsonpath("body-json.action.display[?(@.translationKey == 'action_move_card_from_list_to_list')]")
+                .to("direct:Slushy.WebHook.Trello.MoveCard")
+
+                .otherwise()
+                .setHeader("actionKey").jsonpath("body-json.action.display.translationKey")
                 .process(exchange ->
-                        log.info(exchange.getIn().getBody(String.class)))
+                        log.info(String.format("Action Key: %s", exchange.getIn().getHeader("ActionKey"))))
+                .end()
+        ;
+
+        from("direct:Slushy.WebHook.Trello.MoveCard")
+                .routeId("Slushy.WebHook.Trello.MoveCard")
+                .setHeader("SlushyCardName").jsonpath("body-json.action.data.card.name")
+                .setHeader("SlushyMovedFrom").jsonpath("body-json.action.data.listBefore.name")
+                .setHeader("SlushyMovedTo").jsonpath("body-json.action.data.listAfter.name")
+                .log("Moved Card '${header.SlushyCardName}' from '${header.SlushyMovedFrom}' to '${header.SlushyMovedTo}'")
         ;
     }
 }
