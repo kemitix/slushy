bb9ddbf0c3db42e6367d0ccd2bec3ea8faf34f96
Paul Campbell <pcampbell@kemitix.net>
2020-11-19 08:30:33 +0000

    app.trello.queue: test for missing listBefore/listAfter

--- slushy-app/src/main/java/net/kemitix/slushy/app/trello/queue/WebHookTrelloRoute.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/trello/queue/WebHookTrelloRoute.java
@@ -32,15 +32,25 @@ public class WebHookTrelloRoute
                 .setHeader("actionKey").jsonpath("body-json.action.display.translationKey")
                 .process(exchange ->
                         log.info(String.format("Action Key: %s", exchange.getIn().getHeader("ActionKey"))))
-                .end()
+
+                .endChoice()
         ;
 
         from("direct:Slushy.WebHook.Trello.MoveCard")
                 .routeId("Slushy.WebHook.Trello.MoveCard")
                 .setHeader("SlushyCardName").jsonpath("body-json.action.data.card.name")
+
+                .choice()
+
+                .when().jsonpath("body-json.action.data[?(@.listBefore empty false && @.listAfter empty false)]")
                 .setHeader("SlushyMovedFrom").jsonpath("body-json.action.data.listBefore.name")
                 .setHeader("SlushyMovedTo").jsonpath("body-json.action.data.listAfter.name")
                 .log("Moved Card '${header.SlushyCardName}' from '${header.SlushyMovedFrom}' to '${header.SlushyMovedTo}'")
+
+                .otherwise()
+                .log("Card moved, but listBefore and/or listAfter not given")
+
+                .endChoice()
         ;
     }
 }
