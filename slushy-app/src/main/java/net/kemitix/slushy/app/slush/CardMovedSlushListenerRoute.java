beb65ac3fd957c1f37c06630912c6e1d0faab1ff
Paul Campbell <pcampbell@kemitix.net>
2020-11-22 17:20:56 +0000

    slush: handle when card removed from Slush

---
+++ slushy-app/src/main/java/net/kemitix/slushy/app/slush/CardMovedSlushListenerRoute.java
@@ -0,0 +1,26 @@
+package net.kemitix.slushy.app.slush;
+
+import net.kemitix.slushy.app.reader.ReaderConfig;
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+// listens for card moves and accepts when card is moved removed from the Slush list
+@ApplicationScoped
+public class CardMovedSlushListenerRoute
+        extends RouteBuilder {
+
+    @Inject ReaderConfig readerConfig;
+
+    @Override
+    public void configure() throws Exception {
+        from("seda:Slushy.WebHook.CardMoved?multipleConsumers=true")
+                .routeId("Slushy.WebHook.CardMoved.Slush")
+                .setHeader("ListSlush").constant(readerConfig.getTargetList())
+                .filter().simple("${header.SlushyMovedFrom} == ${header.ListSlush}")
+                .log("Card '${header.SlushyCardName}' removed from ${header.ListSlush}")
+                .to("direct:Slushy.Reader")
+        ;
+    }
+}
