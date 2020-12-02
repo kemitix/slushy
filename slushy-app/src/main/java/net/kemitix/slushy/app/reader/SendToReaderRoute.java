3f00ca982ab27da726a144d2f639edd616d225c3
Paul Campbell <pcampbell@kemitix.net>
2020-12-02 21:00:04 +0000

    app.reader: prevent sending duplicates of card attachment
    Limit cache size to the max size of the slush queue to allow deliberate resending. This is only to prevent a times and a webhook triggered event from sending the same attachment twice.

--- slushy-app/src/main/java/net/kemitix/slushy/app/reader/SendToReaderRoute.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/reader/SendToReaderRoute.java
@@ -8,20 +8,25 @@ import org.apache.camel.builder.RouteBuilder;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import static org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository.memoryIdempotentRepository;
+
 @ApplicationScoped
 public class SendToReaderRoute
         extends RouteBuilder {
     private final SlushyConfig slushyConfig;
+    private final ReaderConfig readerConfig;
     private final SendEmailAttachment sendEmailAttachment;
     private final AddComment addComment;
 
     @Inject
     public SendToReaderRoute(
             SlushyConfig slushyConfig,
+            ReaderConfig readerConfig,
             SendEmailAttachment sendEmailAttachment,
             AddComment addComment
     ) {
         this.slushyConfig = slushyConfig;
+        this.readerConfig = readerConfig;
         this.sendEmailAttachment = sendEmailAttachment;
         this.addComment = addComment;
     }
@@ -30,6 +35,11 @@ public class SendToReaderRoute
     public void configure() {
         from("direct:Slushy.SendToReader")
                 .routeId("Slushy.SendToReader")
+
+                .idempotentConsumer().simple("${header.SlushyCard.id}")
+                .skipDuplicate(true)
+                .messageIdRepository(() ->
+                        memoryIdempotentRepository(readerConfig.getMaxSize()))
 
                 .setHeader("SlushyRecipient").constant(slushyConfig.getReader())
                 .setHeader("SlushySender").constant(slushyConfig.getSender())
