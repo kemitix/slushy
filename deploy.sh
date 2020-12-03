34cebb01121b08e7ce0d91c9b0da474525c3007c
Paul Campbell <pcampbell@kemitix.net>
2020-12-03 06:35:19 +0000

    deploy: add support for selecting version to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,13 @@ git checkout master
 git reset --hard
 git pull origin master
 
+if [ $# -gt 0 ];then
+    echo "Deploying $1"
+    git co $1
+else
+    echo "Deploying latest master"
+fi
+
 mvn install
 docker build -f slushy/src/main/docker/Dockerfile -t kemitix/slushy slushy
 
