dd82e4eded8a5e5eb2592f2e72bd0efb6f39fd85
Paul Campbell <pcampbell@kemitix.net>
2021-04-03 16:59:08 +0100

    InboxSendEmailRoute: only send confirmation email once

--- slushy-app/src/main/java/net/kemitix/slushy/app/inbox/InboxSendEmailRoute.java
+++ slushy-app/src/main/java/net/kemitix/slushy/app/inbox/InboxSendEmailRoute.java
@@ -1,18 +1,40 @@
 package net.kemitix.slushy.app.inbox;
 
+import com.julienvey.trello.domain.Action;
+import com.julienvey.trello.domain.Argument;
+import net.kemitix.slushy.app.SlushyHeader;
+import net.kemitix.trello.TrelloCard;
+import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class InboxSendEmailRoute
         extends RouteBuilder {
+
+    private static final Argument FILTER_COMMENTS =
+            new Argument("filter", "commentCard");
+    private static final String EMAIL_SENT_SIGNATURE = "Sent inbox email to author";
+
     @Override
     public void configure() {
         from("direct:Slushy.Inbox.SendEmailConfirmation")
                 .routeId("Slushy.Inbox.SendEmailConfirmation")
+                .filter(excludeWhenConfirmationAlreadySent())
                 .setHeader("SlushyEmailTemplate").constant("inbox")
                 .to("direct:Slushy.SendEmail")
         ;
+    }
+
+    private Predicate excludeWhenConfirmationAlreadySent() {
+        return exchange -> exchange.getIn()
+                .getHeader(SlushyHeader.CARD, TrelloCard.class)
+                .getActions(FILTER_COMMENTS)
+                .stream()
+                .map(Action::getData)
+                .map(Action.Data::getText)
+                .noneMatch(EMAIL_SENT_SIGNATURE::equals);
     }
 }
